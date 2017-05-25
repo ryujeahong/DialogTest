d40a54b2e91a6e348018efba6e5a47ee22948d3a
ryujeahong <phnin1106@naver.com>
2017-05-25 20:50:33 +0900

    #3 Change Items Dialog

--- app/src/main/java/kr/hs/phjin1106e_mirim/dialogtest/MainActivity.java
+++ app/src/main/java/kr/hs/phjin1106e_mirim/dialogtest/MainActivity.java
@@ -1,24 +1,37 @@
 package kr.hs.phjin1106e_mirim.dialogtest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] itemArr= {"RYUJEHONG", "EscA", "TOBI", "MIRO", "Zunba", "GIDO", "WHORU"};
+    Button butDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog = (Button)findViewById(R.id.but_dialog);
+        butDialog = (Button)findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
-
     @Override
     public void onClick(View view) {
-
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Lunatic_Hai");
+        dialog.setIcon(R.drawable.lunatic);
+     // dialog.setMessage("여기는 메세지를 쓰는 곳입니다");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        dialog.setPositiveButton("OK",null);
+        dialog.show();
     }
 }
